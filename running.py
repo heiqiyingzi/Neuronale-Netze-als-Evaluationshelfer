bdf93f062f388170d2edc4742ec5e67de83ceb24
Ying Meng <uqjol@student.kit.edu>
2021-09-30 16:37:43 +0200

    running

---
+++ running.py
@@ -0,0 +1,5 @@
+import os
+os.system('python ./01_preprocess.py')
+os.system('python ./02_dnn_training.py')
+os.system('python ./03_questionnaire_evaluation.py')
+os.system('python ./04_visualization.py')
